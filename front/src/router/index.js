065ff9024d97be9f5c8294f433d5a91c3f738f7b
VuthyWA <vuthyyibthy@gmail.com>
2021-12-21 20:51:11 +0700

    admin page

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,17 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import View from './../components/views/Example.vue'
+import Admin from '../components/admin/Admin'
 Vue.use(VueRouter)
 
 const routes = [
-    { path: "/admin", component: View },
+    { path: "/admin", component: Admin },
     { path: "/ero_officers", component: View },
     { path: "/alumni_profile", component: View },
     { path: "/alumni_users", component: View },
     // { path: "/alumnis", component: AlumniView },
     // { path: "/alumni/profile", component: ProfileAlumniView },
 
-    { path: "/", redirect: "/alumnis" },
+    { path: "/", redirect: "/admin" },
   
 ]
 
