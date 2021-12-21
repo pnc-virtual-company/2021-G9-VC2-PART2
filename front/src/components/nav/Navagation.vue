e619c8c0ce04279a18d7dde40e38185b4a032868
somphorsngoun <somphors.ngoun@student.passerellesnumeriques.org>
2021-12-21 20:52:39 +0700

    testing

--- front/src/components/nav/Navagation.vue
+++ front/src/components/nav/Navagation.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- <v-app app>  -->
       <v-app-bar class="" width="100%" height="70" app color="white">
-        <v-img src="https://o.remove.bg/downloads/941ec1ae-e883-4c79-b747-af0987068cb1/PN-Cambodia-Alumni-Association__1_-removebg-preview.png" width="120"></v-img>
+        <v-img src="https://www.passerellesnumeriques.org/wp-content/uploads/2019/03/PN-Cambodia-Alumni-Association.png" width="120"></v-img>
         <v-spacer></v-spacer>
         <v-tabs class="mx-auto d-flex justify-center align-center" width="100%" color="#FF9933">
           <v-tab :to="{path: '/admin'}" class="text-h6 blue--text" v-if="role === 'admin'">Administrator</v-tab>
